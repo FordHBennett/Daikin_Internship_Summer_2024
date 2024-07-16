a429901d4dba5220602e09714ab7f746dcfbe1f5
Ford Hideo Bennett <ford660bennett@tamu.edu>
2024-07-16 15:09:44 -0500

    Update tag_generator.ps1

--- tag_generator.ps1
+++ tag_generator.ps1
@@ -1,15 +1,38 @@
+# Possible directories where Python might be installed
+$possiblePaths = @(
+    "$env:LOCALAPPDATA\Programs\Python",
+    "$env:PROGRAMFILES\Python",
+    "$env:PROGRAMFILES(x86)\Python"
+)
+
+# Search for python.exe
+$pythonPath = $null
+foreach ($path in $possiblePaths) {
+    if (Test-Path "$path\python.exe") {
+        $pythonPath = "$path\python.exe"
+        break
+    }
+}
+
+if ($pythonPath -ne $null) {
+    Write-Host "Python found at: $pythonPath"
+} else {
+    Write-Host "Python executable not found in common directories." -ForegroundColor Red
+}
+
+
 try {
-    python.exe -m venv .venv\
+    $pythonPath -m venv .venv\
 } catch {
     Write-Host "An error occurred: $($_)" -ForegroundColor Red
     Write-Host "Ensure that Python is installed and added to the PATH environment variable." -ForegroundColor Red
     exit 1
 }
 .venv\Scripts\activate
-python.exe -m pip install --upgrade pip
-python.exe -m pip install .
+$pythonPath -m pip install --upgrade pip
+$pythonPath -m pip install .
 try {
-    python.exe -m tag_generator -00 --enable-optimizations --with-lto=full --without-doc-strings 2>&1 | % { Write-Host $_ -NoNewline }
+    $pythonPath -m tag_generator -00 --enable-optimizations --with-lto=full --without-doc-strings 2>&1 | % { Write-Host $_ -NoNewline }
     Write-Host "`nIgnition tags generated successfully."
     Write-Host "Check the files/output folder for the generated tags."
 } catch {
